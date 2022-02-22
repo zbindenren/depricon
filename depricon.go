ae5643a8d39dc8ba25655e8241be21f1267c709e
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-02-22 07:44:48 +0100

    add inital commit

---
+++ depricon.go
@@ -0,0 +1,7 @@
+// Package depricon is used to play with deprecated go modules.
+package depricon
+
+// Depricate does stuff
+func Depricate() string {
+	return "i am deprecated"
+}
---
+++ go.mod
@@ -0,0 +1,4 @@
+// Deprecated: This module is not maintained anymore.
+module github.com/zbindenren/depricon
+
+go 1.17
